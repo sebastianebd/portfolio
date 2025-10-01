6e9864b0e66db58b6debd66288a688bc94574f01
Sebastian <sebastian.ebd@gmail.com>
2025-10-01 18:02:17 -0300

    se añade responsive

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,35 @@ import NavBar from "@/components/NavBar";
 
 export default function Home() {
   return (
-    <div className="h-screen bg-[var(--color-bg)]">
+    <div className="relative h-screen bg-[var(--color-bg)] overflow-hidden">
       <NavBar/>
-        HOME Sebastián
+      <div className="absolute w-[300%] md:w-[200%] h-110 bg-[var(--color-fringe)] top-1/2 left-[-30%] rotate-120 z-0"></div>
+
+      <div className="relative z-10 flex md:justify-center h-full">
+
+        <div className="text-white lg:w-1/2 h-full flex flex-col lg:justify-center gap-15 lg:gap-20 pt-15 px-5 lg:pl-60 lg:pb-40">
+          <h1 className="text-4xl md:text-5xl font-bold leading-snug">
+            HOLA, SOY SEBASTIÁN! <br /> ANALISTA PROGRAMADOR
+          </h1>
+          <p className="text-sm md:text-base pr-0 md:pr-35 text-gray-500 font-bold">Soy un apasionado diseñador de UI/UX con la misión de crear experiencias digitales atractivas e 
+            intuitivas. Con una sólida base en principios de diseño y un ojo para el detalle, 
+            me especializo en convertir ideas complejas en interfaces intuitivas que cautivan y enganchan.</p>
+
+          <a 
+            href="/CV_Sebastian.pdf" 
+            download 
+            className="bg-amber-500 hover:bg-[var(--color-navbar)] hover:text-white text-black font-bold 
+            py-2 px-6 transition-colors duration-450 h-16 w-50 text-center pt-5">
+            Descargar CV
+            <img src="../public/download-icon.svg" alt="" />
+          </a>
+        </div>
+
+        <div className=" w-1/2 h-full hidden lg:block ">
+          
+        </div>
+      </div>
+
     </div>
   );
 }
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -2,17 +2,18 @@ import React from 'react';
 
 const NavBar = () => {
   return (
-    <nav className='top-0 h-15 md:h-20 w-full max-w-6xl mx-auto flex bg-[var(--color-navbar)] text-white font-bold rounded'>
+    <nav className='relative top-0 h-15 md:h-20 w-full max-w-6xl mx-auto flex bg-[var(--color-navbar)] text-white 
+                    font-bold rounded z-20'>
 
-      <div className='bg-indigo-600 flex-shrink-0  w-[120px] md:w-[250px] flex items-center justify-center'>
+      <div className='bg-amber-500 text-black  flex-shrink-0  w-[120px] md:w-[250px] flex items-center justify-center '>
         <a href="#home" className='text-sm md:text-lg'>SEBASTIÁN</a>
       </div>
 
-      <div className='flex-1 flex justify-evenly items-center gap-4 sm:gap-4 md:gap-5 lg:gap-7 px-3'>
-        <a href="#home" className='text-xs md:text-base lg:text-lg'>Home</a>
-        <a href="#about" className='text-xs md:text-base lg:text-lg'>About</a>
-        <a href="#projects" className='text-xs md:text-base lg:text-lg'>Projects</a>
-        <a href="#contact" className='text-xs md:text-base lg:text-lg'>Contact</a>
+      <div className='flex-1 flex justify-evenly items-center gap-4 sm:gap-4 md:gap-5 lg:gap-7 px-3 '>
+        <a href="#home" className='hidden md:text-base lg:text-lg hover:-translate-y-1.5 transition-all duration-300 hover:rotate-1 hover:text-amber-400'>Home</a>
+        <a href="#about" className='hidden md:text-base lg:text-lg hover:-translate-y-1.5 transition-all duration-300 hover:rotate-1 hover:text-amber-400'>About</a>
+        <a href="#projects" className='hidden md:text-base lg:text-lg hover:-translate-y-1.5 transition-all duration-300 hover:rotate-1 hover:text-amber-400'>Projects</a>
+        <a href="#contact" className='hidden md:text-base lg:text-lg hover:-translate-y-1.5 transition-all duration-300 hover:rotate-1 hover:text-amber-400'>Contact</a>
       </div>
 
     </nav>
