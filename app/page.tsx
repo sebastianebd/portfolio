b3bcb5dfc351ac2fc80eecbc72e9b98b59ee3257
Sebastian <sebastian.ebd@gmail.com>
2025-10-01 22:42:52 -0300

    se upgradea diseño responsivo mobile

--- app/page.tsx
+++ app/page.tsx
@@ -9,9 +9,11 @@ export default function Home() {
       <div className="relative z-10 flex md:justify-center h-full">
 
         <div className="text-white lg:w-1/2 h-full flex flex-col lg:justify-center gap-15 lg:gap-20 pt-15 px-5 lg:pl-60 lg:pb-40">
+          
           <h1 className="text-4xl md:text-5xl font-bold leading-snug">
             HOLA, SOY SEBASTIÁN! <br /> ANALISTA PROGRAMADOR
           </h1>
+
           <p className="text-sm md:text-base pr-0 md:pr-35 text-gray-500 font-bold">Soy un apasionado diseñador de UI/UX con la misión de crear experiencias digitales atractivas e 
             intuitivas. Con una sólida base en principios de diseño y un ojo para el detalle, 
             me especializo en convertir ideas complejas en interfaces intuitivas que cautivan y enganchan.</p>
@@ -20,10 +22,10 @@ export default function Home() {
             href="/CV_Sebastian.pdf" 
             download 
             className="bg-amber-500 hover:bg-[var(--color-navbar)] hover:text-white text-black font-bold 
-            py-2 px-6 transition-colors duration-450 h-16 w-50 text-center pt-5">
-            Descargar CV
-            <img src="../public/download-icon.svg" alt="" />
+            transition-colors duration-450 h-16 w-50 flex flex-row justify-center items-center">
+            Descargar CV <img src="/download-icon.svg" alt="" width={20} height={20} className="ml-2" />
           </a>
+
         </div>
 
         <div className=" w-1/2 h-full hidden lg:block ">
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,21 +1,53 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
 
 const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <nav className='relative top-0 h-15 md:h-20 w-full max-w-6xl mx-auto flex bg-[var(--color-navbar)] text-white 
-                    font-bold rounded z-20'>
+                    font-bold rounded z-30'>
 
-      <div className='bg-amber-500 text-black  flex-shrink-0  w-[120px] md:w-[250px] flex items-center justify-center '>
-        <a href="#home" className='text-sm md:text-lg'>SEBASTIÁN</a>
+      <div className='bg-amber-500 text-black  flex-shrink-0  w-16 md:w-[250px] flex items-center justify-center '>
+        <img src="/code-icon.svg" alt="terminal icon" height={35} width={35}/>
       </div>
 
-      <div className='flex-1 flex justify-evenly items-center gap-4 sm:gap-4 md:gap-5 lg:gap-7 px-3 '>
-        <a href="#home" className='hidden md:text-base lg:text-lg hover:-translate-y-1.5 transition-all duration-300 hover:rotate-1 hover:text-amber-400'>Home</a>
-        <a href="#about" className='hidden md:text-base lg:text-lg hover:-translate-y-1.5 transition-all duration-300 hover:rotate-1 hover:text-amber-400'>About</a>
-        <a href="#projects" className='hidden md:text-base lg:text-lg hover:-translate-y-1.5 transition-all duration-300 hover:rotate-1 hover:text-amber-400'>Projects</a>
-        <a href="#contact" className='hidden md:text-base lg:text-lg hover:-translate-y-1.5 transition-all duration-300 hover:rotate-1 hover:text-amber-400'>Contact</a>
+      <div className=' flex justify-center items-center pl-5'>
+        <a href="#home" className='text-sm md:text-lg tracking-wider'>SEBASTIÁN</a>
       </div>
 
+      {/* Menú Desktop */}
+      <div className='hidden flex-1 justify-evenly items-center gap-4 sm:gap-4 md:gap-5 lg:gap-7 px-3 '>
+        <a href="#home" className=' md:text-base lg:text-lg hover:-translate-y-1.5 transition-all duration-300 hover:rotate-1 hover:text-amber-400'>Home</a>
+        <a href="#about" className=' md:text-base lg:text-lg hover:-translate-y-1.5 transition-all duration-300 hover:rotate-1 hover:text-amber-400'>About</a>
+        <a href="#projects" className=' md:text-base lg:text-lg hover:-translate-y-1.5 transition-all duration-300 hover:rotate-1 hover:text-amber-400'>Projects</a>
+        <a href="#contact" className=' md:text-base lg:text-lg hover:-translate-y-1.5 transition-all duration-300 hover:rotate-1 hover:text-amber-400'>Contact</a>
+      </div>
+
+      {isOpen && (
+        <>
+          <div 
+            className="fixed inset-0 backdrop-blur-xs"
+            onClick={() => setIsOpen(false)} // Cerrar al hacer click afuera
+          ></div>
+
+          {/* Menú Mobile */}
+          <div className="absolute top-full left-0 w-full bg-[var(--color-bg)] flex flex-col items-center 
+                          py-4 md:hidden border-t border-b border-amber-500 animate-openMenu">
+            <a href="#home" className="py-2 text-lg" onClick={() => setIsOpen(false)}>Home</a>
+            <a href="#about" className="py-2 text-lg" onClick={() => setIsOpen(false)}>About</a>
+            <a href="#projects" className="py-2 text-lg" onClick={() => setIsOpen(false)}>Projects</a>
+            <a href="#contact" className="py-2 text-lg" onClick={() => setIsOpen(false)}>Contact</a>
+          </div>
+        </>
+      )}
+
+      <button
+        className="md:hidden text-2xl focus:outline-none pl-35 text-amber-500"
+        onClick={() => setIsOpen(!isOpen)}
+        aria-label="Menu"
+      >
+        ☰
+      </button>
     </nav>
   );
 }
