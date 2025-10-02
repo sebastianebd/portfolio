dc3e0a9287e74f04606576b144044adba581d678
Sebastian <sebastian.ebd@gmail.com>
2025-10-02 01:53:24 -0300

    se configura responsive tablet

--- app/page.tsx
+++ app/page.tsx
@@ -2,19 +2,20 @@ import NavBar from "@/components/NavBar";
 
 export default function Home() {
   return (
-    <div className="relative h-screen bg-[var(--color-bg)] overflow-hidden">
+    <div className="relative h-screen md:h-170 lg:h-screen bg-[var(--color-bg)] overflow-hidden">
       <NavBar/>
-      <div className="absolute w-[300%] md:w-[200%] h-110 bg-[var(--color-fringe)] top-1/2 left-[-30%] rotate-120 z-0"></div>
+      <div className="absolute w-[300%] md:w-[200%] h-110 md:h-80 lg:h-110 bg-[var(--color-fringe)] top-1/2 left-[-30%] rotate-120 z-0"></div>
 
       <div className="relative z-10 flex md:justify-center h-full">
 
-        <div className="text-white lg:w-1/2 h-full flex flex-col lg:justify-center gap-15 lg:gap-20 pt-15 px-5 lg:pl-60 lg:pb-40">
+        <div className="text-white lg:w-1/2 h-full flex flex-col lg:justify-center gap-15 lg:gap-20 pt-15 px-5 lg:pl-60 
+                        pb-15 md:px-15 lg:pb-40">
           
           <h1 className="text-4xl md:text-5xl font-bold leading-snug">
             HOLA, SOY SEBASTIÁN! <br /> ANALISTA PROGRAMADOR
           </h1>
 
-          <p className="text-sm md:text-base pr-0 md:pr-35 text-gray-500 font-bold">Soy un apasionado diseñador de UI/UX con la misión de crear experiencias digitales atractivas e 
+          <p className="text-sm md:text-base pr-0 text-gray-500 font-bold">Soy un apasionado diseñador de UI/UX con la misión de crear experiencias digitales atractivas e 
             intuitivas. Con una sólida base en principios de diseño y un ojo para el detalle, 
             me especializo en convertir ideas complejas en interfaces intuitivas que cautivan y enganchan.</p>
 
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -7,16 +7,16 @@ const NavBar = () => {
     <nav className='relative top-0 h-15 md:h-20 w-full max-w-6xl mx-auto flex bg-[var(--color-navbar)] text-white 
                     font-bold rounded z-30'>
 
-      <div className='bg-amber-500 text-black  flex-shrink-0  w-16 md:w-[250px] flex items-center justify-center '>
+      <div className='bg-amber-500 text-black  flex-shrink-0  w-16 md:w-26 flex items-center justify-center '>
         <img src="/code-icon.svg" alt="terminal icon" height={35} width={35}/>
       </div>
 
       <div className=' flex justify-center items-center pl-5'>
-        <a href="#home" className='text-sm md:text-lg tracking-wider'>SEBASTIÁN</a>
+        <a href="#home" className='text-md md:text-lg tracking-wider'>SEBASTIÁN</a>
       </div>
 
       {/* Menú Desktop */}
-      <div className='hidden flex-1 justify-evenly items-center gap-4 sm:gap-4 md:gap-5 lg:gap-7 px-3 '>
+      <div className='hidden md:flex flex-1 justify-evenly items-center gap-4 sm:gap-4 md:gap-5 lg:gap-7 px-3 '>
         <a href="#home" className=' md:text-base lg:text-lg hover:-translate-y-1.5 transition-all duration-300 hover:rotate-1 hover:text-amber-400'>Home</a>
         <a href="#about" className=' md:text-base lg:text-lg hover:-translate-y-1.5 transition-all duration-300 hover:rotate-1 hover:text-amber-400'>About</a>
         <a href="#projects" className=' md:text-base lg:text-lg hover:-translate-y-1.5 transition-all duration-300 hover:rotate-1 hover:text-amber-400'>Projects</a>
@@ -42,7 +42,7 @@ const NavBar = () => {
       )}
 
       <button
-        className="md:hidden text-2xl focus:outline-none pl-35 text-amber-500"
+        className="ml-auto mr-8 md:hidden text-2xl focus:outline-none text-amber-500"
         onClick={() => setIsOpen(!isOpen)}
         aria-label="Menu"
       >
